47d88bd5f891337533db6244d2fc4a269a16fd37
Jean-Michel Deruty <nia.elfette@live.fr>
2023-07-25 15:49:27 +0200

    Add test project to solution
    Work on Dependency resolver

--- src/dotnet/Stormancer.Client/Stormancer.Client/StormancerResolver.cs
+++ src/dotnet/Stormancer.Client/Stormancer.Client/StormancerResolver.cs
@@ -1,58 +1,265 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 
 namespace Stormancer
 {
     /// <summary>
-    /// Dependency scope
+    /// A Scope for dependencies 
     /// </summary>
-    public interface IDependencyScope
+    public class DependencyScope: IDisposable
     {
-        T Resolve<T>() where T : class;
-        T? ResolveOptional<T>() where T : class;
-        IEnumerable<T> ResolveAll<T>() where T : class;
-        bool TryResolve<T>(out T? dependency) where T : class;
-       
-      
+        private class Dependency
+        {
+            public Dependency(Registration registration) 
+            {
+                Registration = registration;
+            }
+            public object? Instance { get; set; }
+            public Registration Registration { get; }
+        }
+        private readonly IEnumerable<Registration> _registrations;
+        private readonly DependencyScope? _parent;
+
+        private readonly Dictionary<Type, List<Dependency>> _dependencies = new Dictionary<Type, List<Dependency>>();
+        internal DependencyScope(IEnumerable<Registration> registrations) 
+        {
+            _registrations = registrations;
+        }
+
+        internal DependencyScope(DependencyScope parent,  IEnumerable<Registration> registrations)
+        {
+            _registrations = registrations;
+            _parent = parent;
+        }
+        /// <summary>
+        /// Resolves a mandatory dependency.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <returns></returns>
+        public T Resolve<T>() where T : class
+        {
+
+        }
+
+        /// <summary>
+        /// Resolves an optional dependency.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <returns></returns>
+        public T? ResolveOptional<T>() where T : class
+        {
+
+        }
+
+        /// <summary>
+        /// Resolves all dependencies satisfying a given contract.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <returns></returns>
+        public IEnumerable<T> ResolveAll<T>() where T : class
+        {
+
+        }
+
+        /// <summary>
+        /// Tries resolving a dependency satisfying a given contract, and returns false if it couldn't be resolved.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="dependency"></param>
+        /// <returns></returns>
+        public bool TryResolve<T>([NotNullWhen(true)]out T? dependency) where T : class
+        {
+
+        }
+
+        /// <summary>
+        /// Creates a child scope of the current scope, that inherits registrations and 
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="configurator"></param>
+        /// <returns></returns>
+        public DependencyScope CreateChild(string name, Action<DependencyBuilder>? configurator = null)
+        {
+            if(configurator == null)
+            {
+                return new DependencyScope(this, Enumerable.Empty<Registration>());
+            }
+            else
+            {
+                var builder = new DependencyBuilder();
+                configurator(builder);
+                return new DependencyScope(this, builder.Registrations);
+            }
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
     }
 
+    /// <summary>
+    /// Provides methods to configure dependencies in a scope.
+    /// </summary>
     public class DependencyBuilder
     {
+        /// <summary>
+        /// Registers a new dependency type by providing a factory method.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="component"></param>
+        /// <returns></returns>
         public Registration Register<T>(Func<T> component) where T : class
         {
             return Register(_ => component());
         }
-        public Registration<T> Register<T>(Func<IDependencyScope, T> component) where T : class
-        {
 
-        }
-        public Registration<T> RegisterInstance<T>(T component) where T : class
+        /// <summary>
+        /// Registers a new dependency type by providing a factory method.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="component"></param>
+        /// <returns></returns>
+        public Registration<T> Register<T>(Func<DependencyScope, T> component) where T : class
         {
-
+            var registration = new Registration<T>(component);
+            Registrations.Add(registration);
+            return registration;
         }
+
+        /// <summary>
+        /// Registers a dependency as an already created instance.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="component"></param>
+        /// <returns></returns>
+        public Registration<T> Register<T>(T component) where T : class
+        {
+            var registration = new Registration<T>(component);
+            Registrations.Add(registration);
+            return registration;
+        }
+
+        internal List<Registration> Registrations { get; } = new List<Registration>();
     }
+    
+    /// <summary>
+    /// Base class for registrations in the dependency injection system.
+    /// </summary>
     public class Registration
     {
-
-    }
-    public class Registration<T> : Registration where T : class
-    {
-
-        public Func<IDependencyScope, object>? factory;
-        public bool singleInstance;
-        public object? instance;
-
-        public Registration(Func<IDependencyResolver, object>? factory, bool singleInstance, object? instance)
+        internal Registration(Type implementationType)
         {
-            this.factory = factory;
-            this.singleInstance = singleInstance;
-            this.instance = instance;
+            ImplementationType = implementationType;
         }
 
-        public Registration As<TService>()
+        /// <summary>
+        /// Instance provided when creating the registration if it exists.
+        /// </summary>
+        protected object? _instance;
+
+        /// <summary>
+        /// Factory used to create new instances of the registration if it exists.
+        /// </summary>
+        /// <remarks>
+        /// <see cref="_instance"/> and <see cref="_factory"/> are mutually exclusive.
+        /// </remarks>
+        protected Func<DependencyScope, object>? _factory;
+
+        /// <summary>
+        /// Gets the type of the objects created by the registration.
+        /// </summary>
+        public Type ImplementationType { get; }
+
+        /// <summary>
+        /// Gets the list of services provided by the registration.
+        /// </summary>
+        public List<Type> ServicesTypes { get; } = new List<Type>();
+
+        /// <summary>
+        /// Gets a value indicating if the registration should be a single instance per scope.
+        /// </summary>
+        public bool SingleInstance { get; protected set; }
     }
 
+    /// <summary>
+    /// A registration in the Dependency injection system.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class Registration<T> : Registration where T : class
+    {
+        internal TService? GetInstance<TService>(DependencyScope scope) where TService:class
+        {
+            if (_instance != null)
+            {
+                return (TService)_instance;
+            }
+            else
+            {
+                return (TService?)_factory?.Invoke(scope);
+            }
+        }
+
+
+        internal Registration(Func<DependencyScope, T>? factory) : base(typeof(T))
+        {
+            _factory = factory;
+         
+        }
+
+        internal Registration(T instance) : base(typeof(T))
+        {
+            _instance = instance;
+         
+        }
+
+        /// <summary>
+        /// Adds a <typeparamref name="TService"/> as a contract for <typeparamref name="T"/>
+        /// </summary>
+        /// <typeparam name="TService"></typeparam>
+        /// <returns></returns>
+        /// <exception cref="InvalidCastException"></exception>
+        public Registration<T> As<TService>() where TService :  class
+        {
+            if(typeof(TService).IsAssignableFrom(typeof(T)))
+            {
+                ServicesTypes.Add(typeof(TService));
+                return this;
+            }
+            else
+            {
+                throw new InvalidCastException($"'{typeof(T)}' does not implement '{typeof(TService)}'");
+            }
+        }
+    }
+
+  
+    internal class Container : IDisposable
+    {
+        private DependencyScope? _rootScope;
+        public IEnumerable<Registration> Registrations { get; }
+        public Container(Action<DependencyBuilder> configurator)
+        {
+            var builder = new DependencyBuilder();
+            configurator(builder);
+            Registrations = builder.Registrations;
+        }
+
+        public DependencyScope CreateRootScope()
+        {
+            _rootScope = new DependencyScope(Registrations);
+            return _rootScope;
+        }
+
+        public void Dispose()
+        {
+            _rootScope?.Dispose();
+        }
+    }
+
+    
     public class StormancerResolver : IDependencyResolver, IDisposable
     {
         private readonly Dictionary<Type, List<Registration>> _registrations = new Dictionary<Type, List<Registration>>();
@@ -64,7 +271,7 @@ namespace Stormancer
             _parent = parent;
         }
 
-       public  T? ResolveOptional<T>() where T : notnull
+        public T? ResolveOptional<T>() where T : notnull
         {
             if (TryResolve<T>(out var result))
             {
